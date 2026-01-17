395bf85807a2df1239ba4f5797df76497970ebae
Gusta-code22 <gustavobritomirandaxbox@gmail.com>
2026-01-16 21:48:02 -0300

    feat: adicionar API Gateway com balanceamento de carga e rotas via RouteLocator

---
+++ api-gateway/src/main/java/github/io/Gusta_code22/config/ApiGatewayConfiguration.java
@@ -0,0 +1,21 @@
+package github.io.Gusta_code22.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+
+        return builder.routes()
+
+                .route(p -> p.path("/service-produto/**").uri("lb://service-produto"))
+                .route(p -> p.path("/service-preco/**").uri("lb://service-preco"))
+                .route(p -> p.path("/service-imposto/**").uri("lb://service-imposto"))
+                .build();
+    }
+}
