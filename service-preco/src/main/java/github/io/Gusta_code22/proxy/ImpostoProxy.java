6d3bcc7cf0b84d7254a3f862923f0a9ca3ca3354
Gusta-code22 <gustavobritomirandaxbox@gmail.com>
2026-01-22 21:25:25 -0300

    Corrige propagação de erros entre serviços e configura Retry e Circuit Breaker com Resilience4j

--- service-preco/src/main/java/github/io/Gusta_code22/proxy/ImpostoProxy.java
+++ service-preco/src/main/java/github/io/Gusta_code22/proxy/ImpostoProxy.java
@@ -1,6 +1,8 @@
 package github.io.Gusta_code22.proxy;
 
 import github.io.Gusta_code22.dto.ImpostoDTO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,14 @@ import java.math.BigDecimal;
 public interface ImpostoProxy {
 
     @GetMapping(value = "/service-imposto/{valor}/{moeda}")
+    @CircuitBreaker(name = "service-preco", fallbackMethod = "fallbackB")
+    @Retry(name = "service-preco")
     ImpostoDTO getImposto(
             @PathVariable("valor") BigDecimal valor,
             @PathVariable("moeda") String moeda
     );
+
+    default String fallbackB(Exception ex) {
+        return ("Fallback from B → A");
+    }
 }
--- service-produto/src/main/java/github/io/Gusta_code22/proxy/PrecoProxy.java
+++ service-produto/src/main/java/github/io/Gusta_code22/proxy/PrecoProxy.java
@@ -1,6 +1,9 @@
 package github.io.Gusta_code22.proxy;
 
+
 import github.io.Gusta_code22.dto.PrecoDTO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +14,14 @@ import java.math.BigDecimal;
 public interface PrecoProxy {
 
     @GetMapping(value = "/service-preco/{valor}/{moeda}")
+    @CircuitBreaker(name = "service-produto", fallbackMethod = "fallbackA")
+    @Retry(name = "service-produto")
     PrecoDTO getPreco(
             @PathVariable("valor") BigDecimal valor,
             @PathVariable("moeda") String moeda);
+
+
+    default String  fallbackA(Exception ex) {
+        return ("Fallback from A → B");
+    }
 }
